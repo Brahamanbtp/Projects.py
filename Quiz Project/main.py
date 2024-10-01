0157a142fa29a6dbbbf66c4382c81450175755aa
Pranay sharma <116100311+Brahamanbtp@users.noreply.github.com>
2024-10-01 16:30:21 +0000

    initial commit

---
+++ Quiz Project/main.py
@@ -0,0 +1,17 @@
+import Question
+import question_data
+import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question['text']
+    question_answer = question['answer']
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"Thanks for completing that!\nYour final score is: {quiz.score}/{quiz.question_number}")
