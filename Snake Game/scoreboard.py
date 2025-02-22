2ad752265c2a774a58894089471ebfb826bd7ceb
Pranay sharma <116100311+Brahamanbtp@users.noreply.github.com>
2025-02-22 17:39:53 +0000

    code modification

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
-ALIGNMENT = 'center'
-FONT = ('Times New Roman', 24, 'bold')
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard(Turtle):
@@ -8,19 +8,33 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.hideturtle()
-        self.penup()
+        self.high_score = 0
         self.color("white")
+        self.penup()
         self.goto(0, 260)
+        self.hideturtle()
+        self.update_scoreboard()
 
-    def start_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-
-    def increase_scoreboard(self):
+    def update_scoreboard(self):
         self.clear()
-        self.score += 1
-        self.start_scoreboard()
+        self.read_file()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_file()
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def read_file(self):
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+
+    def write_file(self):
+        with open("data.txt", mode="w") as file:
+            file.write(f"{self.high_score}")
